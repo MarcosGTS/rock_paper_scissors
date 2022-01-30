fcfe444868b60826cead9fa6b19aa9a0b38e288e
MarcosGTS <marcosgtsodre@gmail.com>
2022-01-30 09:44:45 -0300

    refactor code to accept new UI

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
 const POSSIBLE_CHOICES = ["rock", "paper", "scissors"];
+const BESTOF = 3;
+const game = {
+    pScore: 0,
+    cScore: 0,
+    bestof: BESTOF,
+}
+
+function setGame() {
+    //reset scores
+    game.cScore = 0;
+    game.pScore = 0;
+
+    const buttons = document.querySelectorAll(".move");
+    buttons.forEach(btn => btn.addEventListener("click", getPlayerMove))   
+}
+
+function getPlayerMove() {
+   
+
+    const playerSelection = this.dataset.move;
+    const computerSelection = computerPlay();
+
+    const roundMessage = playRound(playerSelection, computerSelection);
+    displayResult(roundMessage);
+
+    renderMoves(playerSelection, computerSelection);
+    
+    let winner = ""
+    if (winner = checkWinner()) {
+        setGame();
+        displayResult(`THE GREAT WINNER IS ${winner}`);
+    }  
+}
+
+function checkWinner() {
+    let {cScore, pScore, bestof} = game;
+
+    if (cScore >= bestof)
+        return "BOT";
+    
+    if (pScore >= bestof)
+        return "PLAYER";
+    
+    return "";
+}
 
 function generateRandomNumber(num) {
     return Math.floor(Math.random() * (num + 1)); 
@@ -16,7 +61,6 @@ function playRound(playerSelection, computerSelection) {
     if (!POSSIBLE_CHOICES.includes(playerSelection)) 
         return "Invalid Input";
 
-    
     //Handle tie!
     if (playerSelection === computerSelection) return "Tie";
 
@@ -25,29 +69,35 @@ function playRound(playerSelection, computerSelection) {
         case playerSelection === "paper" && computerSelection === "scissors":
         case playerSelection === "rock" && computerSelection === "paper":
         case playerSelection === "scissors" && computerSelection === "rock":
-            computerScore++;
+            game.cScore++;
             return `You lose! ${computerSelection} beats ${playerSelection}`;
         default:
-            playerScore++;
+            game.pScore++;
             return `You win! ${playerSelection} beats ${computerSelection}`;
     }
         
 }
 
-function getPlayerMove() {
-    const playerSelection = this.dataset.move;
-    const computerSelection = computerPlay();
-    console.log(playRound(playerSelection, computerSelection));
+function displayResult(message) {
+    const result = document.querySelector(".result");
+    result.textContent = message;
 }
 
-//players scores
-let playerScore = 0;
-let computerScore = 0;
+function renderMoves(playerMove, computerMove){
+    let players = document.querySelectorAll(".move-display");
+    let player = players[0];
+    let computer = players[1];
 
-function game() {
-    const buttons = document.querySelectorAll(".move");
+    player.innerHTML = `<img src="./imgs/${playerMove}.svg" alt="${playerMove}">`;
+    computer.innerHTML = `<img src="./imgs/${computerMove}.svg" alt="${computerMove}">`;
 
-    buttons.forEach(btn => btn.addEventListener("click", getPlayerMove))   
+    //scores 
+    let playerScore = document.querySelector(".player-score");
+    let computerScore = document.querySelector(".bot-score");
+
+    //formating
+    playerScore.textContent = `${game.pScore}`.padStart(2, 0);
+    computerScore.textContent = `${game.cScore}`.padStart(2, 0);
 }
 
-game();
+setGame();
